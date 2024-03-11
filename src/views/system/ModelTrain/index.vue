a1327d8b4596a647c62ca199d301afa9492785f7
罗梓轩 <73745830+lzxll@users.noreply.github.com>
2024-03-11 16:43:57 +0800

    111

---
+++ src/views/system/ModelTrain/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <fs-page>
+    <fs-crud ref="crudRef">
+      <template #cell_url="scope">
+        <el-tag size="small">{{ scope.row.url }}</el-tag>
+      </template>
+    </fs-crud>
+
+    <permission ref="rolePermission"></permission>
+  </fs-page>
+</template>
